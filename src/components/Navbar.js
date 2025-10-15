d070333f7c5fd1f695ec42496b1a08f9c56cd18a
Mohammad ladadweh <mohalada1@gmail.com>
2025-10-15 17:40:46 +0300

    Menu-PDF1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -119,7 +119,7 @@ const Navbar = () => {
             Home
           </Link>
           <Link to="/menu" className="nav-link" onClick={closeMobileMenu}>
-            Cafe
+            Menu
           </Link>
           <Link to="/offers" className="nav-link" onClick={closeMobileMenu}>
             Offers
